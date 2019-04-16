f497ba502e6b229483366c3fb08adaf5f1300fe0
Steven Holdsworth <sholdsworth@gmail.com>
2019-04-16 20:44:09 +0100

    winner winner chicken dinner

--- lib/xyozys.ex
+++ lib/xyozys.ex
@@ -1,11 +1,13 @@
 defmodule Xyozys do
-  @type game :: any()
-  @type cell :: any()
-  @type player :: :x | :y
-  @type status :: :tie | :x_wins | :o_wins | :x_turn | :o_turn
-  @type reason :: :game_over | :not_x_turn | :not_y_turn
+  @type game :: list(cell_value)
+  @type cell_value :: :_ | player
+  @type cell :: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+  @type player :: :X | :O
+  @type winner :: :tie | player
+  @type status :: {:over, winner} | {:turn, player}
+  @type reason :: :game_over | :not_players_turn | :cell_already_set
 
-  @empty ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
+  @empty [:_, :_, :_, :_, :_, :_, :_, :_, :_]
 
   @spec new() :: game()
   def new() do
@@ -14,11 +16,126 @@ defmodule Xyozys do
 
   @spec turn(game, cell, player) :: {:ok, game} | {:error, reason}
   def turn(game, cell, player) do
-    :not_implemented
+    case status(game) do
+      {:over, _} ->
+        {:error, :game_over}
+
+      {:turn, ^player} ->
+        case value_at(game, cell) do
+          :_ ->
+            {:ok, set_cell(game, cell, player)}
+
+          _ ->
+            {:error, :cell_already_set}
+        end
+
+      {:turn, _} ->
+        {:error, :not_players_turn}
+    end
   end
 
   @spec status(game) :: status
   def status(game) do
-    :not_implemented
+    with :not_finished <- who_won(game),
+         turn <- whose_turn(game) do
+      turn
+    else
+      {:over, _} = result ->
+        result
+    end
+  end
+
+  @def false
+  @spec who_won(game) :: {:over, :tie | player}
+  def who_won(game) do
+    over(game)
+  end
+
+  @doc false
+  @spec whose_turn(game) :: {:turn, player} | :neither
+  def whose_turn(game)
+
+  def whose_turn(@empty) do
+    {:turn, :X}
+  end
+
+  def whose_turn(game) do
+    xs = Enum.count(game, &(&1 == :X))
+    os = Enum.count(game, &(&1 == :O))
+
+    cond do
+      xs > os -> {:turn, :O}
+      true -> {:turn, :X}
+    end
+  end
+
+  @doc false
+  @spec value_at(game, cell) :: cell_value
+  def value_at(game, cell) do
+    Enum.at(game, cell)
+  end
+
+  @doc false
+  @spec set_cell(game, cell, player) :: game
+  def set_cell(game, cell, player) do
+    List.replace_at(game, cell, player)
+  end
+
+  @doc false
+  @spec over(game) :: {:over, :tie | :player} | :not_finished
+  def over([
+    p, p, p,
+    _, _, _,
+    _, _, _
+  ]) when p !== :_, do: {:over, p}
+
+  def over([
+    _, _, _,
+    p, p, p,
+    _, _, _
+  ]) when p !== :_, do: {:over, p}
+
+  def over([
+    _, _, _,
+    _, _, _,
+    p, p, p
+  ]) when p !== :_, do: {:over, p}
+
+  def over([
+    p, _, _,
+    _, p, _,
+    _, _, p
+  ]) when p !== :_, do: {:over, p}
+
+  def over([
+    _, _, p,
+    _, p, _,
+    p, _, _
+  ]) when p !== :_, do: {:over, p}
+
+  def over([
+    p, _, _,
+    p, _, _,
+    p, _, _
+  ]) when p !== :_, do: {:over, p}
+
+  def over([
+    _, p, _,
+    _, p, _,
+    _, p, _
+  ]) when p !== :_, do: {:over, p}
+
+  def over([
+    _, _, p,
+    _, _, p,
+    _, _, p
+  ]) when p !== :_, do: {:over, p}
+
+  def over(game) do
+    blanks = Enum.count(game, &(&1 == :_))
+    case blanks do
+      0 -> {:over, :tie}
+      _ -> :not_finished
+    end
   end
 end
--- test/xyozys_test.exs
+++ test/xyozys_test.exs
@@ -3,6 +3,119 @@ defmodule XyozysTest do
   doctest Xyozys
 
   test "new should return a new game" do
-    assert Xyozys.new() == ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
+    assert Xyozys.new() == [:_, :_, :_, :_, :_, :_, :_, :_, :_]
   end
+
+  test "status of new board should be X's turn" do
+    assert Xyozys.new() |> Xyozys.status() == {:turn, :X}
+  end
+
+  test "after X plays a turn the cell should be set to X" do
+    assert Xyozys.new()
+           |> Xyozys.turn(0, :X) == {:ok, [:X, :_, :_, :_, :_, :_, :_, :_, :_]}
+  end
+
+  test "after X plays a turn and O tries to set same cell should return error" do
+    assert {:ok, game} = Xyozys.new() |> Xyozys.turn(0, :X)
+    assert Xyozys.turn(game, 0, :O) == {:error, :cell_already_set}
+  end
+
+  test "after X plays a turn it should be O's turn" do
+    assert {:ok, game} =
+             Xyozys.new()
+             |> Xyozys.turn(0, :X)
+
+    assert {:turn, :O} = Xyozys.status(game)
+  end
+
+  @win_scenarios [
+    :win_hori_top,
+    :win_hori_mid,
+    :win_hori_bot,
+    :win_vert_l,
+    :win_vert_mid,
+    :win_vert_r,
+    :win_diag_lr,
+    :win_diag_rl
+  ]
+
+  @tie_scenarios [
+    :tie
+  ]
+
+  test "when X wins X should be the winner" do
+    for win_scenario <- @win_scenarios do
+      game = apply(__MODULE__, win_scenario, [:X])
+      assert Xyozys.status(game) == {:over, :X}
+    end
+  end
+
+  test "when O wins O should be the winner" do
+    for win_scenario <- @win_scenarios do
+      game = apply(__MODULE__, win_scenario, [:O])
+      assert Xyozys.status(game) == {:over, :O}
+    end
+  end
+
+  test "when tie should say so" do
+    for scenario <- @tie_scenarios do
+      game = apply(__MODULE__, scenario, [])
+      assert Xyozys.status(game) == {:over, :tie}
+    end
+  end
+
+  def tie() do
+    [:X, :O, :X,
+     :O, :X, :O,
+     :O, :X, :O]
+  end
+
+  def win_hori_top(pl) do
+    [pl, pl, pl,
+     :_, :_, :_,
+     :_, :_, :_]
+  end
+
+  def win_hori_mid(pl) do
+    [:_, :_, :_,
+      pl, pl, pl,
+     :_, :_, :_]
+  end
+
+  def win_hori_bot(pl) do
+    [:_, :_, :_,
+     :_, :_, :_,
+     pl, pl, pl]
+  end
+
+  def win_vert_l(pl) do
+    [pl, :_, :_,
+     pl, :_, :_,
+     pl, :_, :_]
+  end
+
+  def win_vert_mid(pl) do
+    [:_, pl, :_,
+     :_, pl, :_,
+     :_, pl, :_]
+  end
+
+  def win_vert_r(pl) do
+    [:_, :_, pl,
+     :_, :_, pl,
+     :_, :_, pl]
+  end
+
+  def win_diag_lr(pl) do
+    [pl, :_, :_,
+     :_, pl, :_,
+     :_, :_, pl]
+  end
+
+  def win_diag_rl(pl) do
+    [:_, :_, pl,
+     :_, pl, :_,
+     pl, :_, :_]
+  end
+
 end
